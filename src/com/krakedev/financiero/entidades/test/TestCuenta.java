20de449e30dabdea8ebe921a44464357df460f8a
paulveloz28 <paulveloz28@gmail.com>
2026-08-17 14:26:45 -0500

    test: Crear prueba manual de cuenta

---
+++ src/com/krakedev/financiero/entidades/test/TestCuenta.java
@@ -0,0 +1,15 @@
+package com.krakedev.financiero.entidades.test;
+
+import com.krakedev.financiero.entidades.Cuenta;
+
+public class TestCuenta {
+
+	public static void main(String[] args) {
+
+	        Cuenta cuenta = new Cuenta("1000");
+	        cuenta.imprimir();
+	    }
+
+	}
+
+
