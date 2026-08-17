d5c654f5454232979d7c7315d8e24fe494fa6006
paulveloz28 <paulveloz28@gmail.com>
2026-08-17 11:56:01 -0500

    feat: Agregar constructor de Cliente

--- src/com/krakedev/financiero/entidades/Cliente.java
+++ src/com/krakedev/financiero/entidades/Cliente.java
@@ -1,5 +1,15 @@
 package com.krakedev.financiero.entidades;
 
 public class Cliente {
+	
+	private String cedula;
+    private String nombre;
+    private String apellido;
+    
+    public Cliente(String cedula, String nombre, String apellido) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
 
 }
