5d867c4b14c6c1ad6ee4955eee03f356c8aa40b3
paulveloz28 <paulveloz28@gmail.com>
2026-08-17 12:03:25 -0500

    feat: Agregar getters y setters de cliente

--- src/com/krakedev/financiero/entidades/Cliente.java
+++ src/com/krakedev/financiero/entidades/Cliente.java
@@ -10,6 +10,31 @@ public class Cliente {
         this.cedula = cedula;
         this.nombre = nombre;
         this.apellido = apellido;
+        
     }
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
 
 }
