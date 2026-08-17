cc300448f0a220d9772b617ced657c91925222dd
paulveloz28 <paulveloz28@gmail.com>
2026-08-17 13:43:34 -0500

    feat: Mostrar propietario en imprimir Cuenta

--- src/com/krakedev/financiero/entidades/Cuenta.java
+++ src/com/krakedev/financiero/entidades/Cuenta.java
@@ -5,6 +5,7 @@ public class Cuenta {
 	    private String id;
 	    private double saldoActual;
 	    private String tipo;
+	    private Cliente propietario;
 	    
 	    public Cuenta(String id) {
 	        this.id = id;
@@ -35,8 +36,24 @@ public class Cuenta {
 		public void setTipo(String tipo) {
 			this.tipo = tipo;
 		}
+		
+		public Cliente getPropietario() {
+		    return propietario;
+		}
+
+		public void setPropietario(Cliente propietario) {
+		    this.propietario = propietario;
+		}
 	    
-	    
+		public void imprimir() {
+			System.out.println("ID: " + id);
+		    System.out.println("Saldo actual: " + saldoActual);
+		    System.out.println("Tipo: " + tipo);
+		    System.out.println("Cédula cliente: " + propietario.getCedula());
+		    System.out.println("Nombre cliente: " + propietario.getNombre());
+		    System.out.println("Apellido cliente: " + propietario.getApellido());
+		}
+		
 	}
 
 
