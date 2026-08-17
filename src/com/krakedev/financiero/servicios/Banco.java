01a9fb35507309a4f77def5a319ebb4dd7f0de15
paulveloz28 <paulveloz28@gmail.com>
2026-08-17 15:51:44 -0500

    feat: Implementar metodo crearCuenta

--- src/com/krakedev/financiero/servicios/Banco.java
+++ src/com/krakedev/financiero/servicios/Banco.java
@@ -1,5 +1,8 @@
 package com.krakedev.financiero.servicios;
 
+import com.krakedev.financiero.entidades.Cliente;
+import com.krakedev.financiero.entidades.Cuenta;
+
 public class Banco {
 	
 	private int ultimoCodigo;
@@ -14,6 +17,19 @@ public class Banco {
 
     public void setUltimoCodigo(int ultimoCodigo) {
         this.ultimoCodigo = ultimoCodigo;
+    }
+    
+    public Cuenta crearCuenta(Cliente cliente) {
+
+        String codigoStr = ultimoCodigo + "";
+
+        ultimoCodigo++;
+
+        Cuenta cuenta = new Cuenta(codigoStr);
+
+        cuenta.setPropietario(cliente);
+
+        return cuenta;
     }
 
 }
